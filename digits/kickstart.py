c89bbe7f7a987d47109f69c3b4168d5e6665697a
ganiserb <g.gabriel.b@gmail.com>
2016-08-12 14:48:26 -0300

    changed the way we test by using fold validation

--- digits/kickstart.py
+++ digits/kickstart.py
@@ -2,6 +2,7 @@ import json
 import sys
 from itertools import islice
 from statistics import mean
+from random import shuffle
 
 
 def draw_ascii_image(image):
@@ -24,30 +25,38 @@ def build_prediction(train, test):
     """
     Edit here. Should return one prediction for each element in test.
     """
+    shuffle(train)
+    total = len(train)
+    test_size = int(0.1 * total)
+    train_size = total - test_size
+    test = train[train_size:]
+    train = train[0:train_size]
+
     digits = {}
     average_digits = {}
 
     # Pack all the arrays of each digit into a dict
-    for number in range(10):
-        digits[number] = [digit['image'] for digit in train if digit['label'] == str(number)]
-
-    # Calculate an average matrix for each digit
-    for number in range(10):
-        average_digits[number] = []
-        # Put all bytes of the same position into one list for each
-        for bytes in zip(*digits[number]):
-            # value = 255 if mean(bytes) > 160 else 0  # Make this sort of black and white
-            value = mean(bytes)
-            average_digits[number].append(value)
-
-    with open('average_digits.json', 'w') as file:
-        json.dump(average_digits, file)
+    # for number in range(10):
+    #     digits[number] = [digit['image'] for digit in train if digit['label'] == str(number)]
+    #
+    # # Calculate an average matrix for each digit
+    # for number in range(10):
+    #     average_digits[number] = []
+    #     # Put all bytes of the same position into one list for each
+    #     for bytes in zip(*digits[number]):
+    #         # value = 255 if mean(bytes) > 160 else 0  # Make this sort of black and white
+    #         value = mean(bytes)
+    #         average_digits[number].append(value)
+    #
+    # with open('average_digits.json', 'w') as file:
+    #     json.dump(average_digits, file)
 
     with open('average_digits.json', 'r') as file:
         average_digits = json.load(file)
     # print(average_digits)
 
     result = []
+    corrects = 0
     for sample in test:
 
         # Compute the differences between the sample and the average numbers
@@ -63,6 +72,8 @@ def build_prediction(train, test):
 
         # Get the representation that is least different
         guess = str(min(digit_differences, key=digit_differences.get))
+        if sample['label'] == str(guess):
+            corrects += 1
 
         result.append(guess)
 
@@ -71,6 +82,8 @@ def build_prediction(train, test):
         #
         # for sample_line, average_line in zip(sample_image, average_image):
         #     print(sample_line + average_line)
+
+    print(corrects / len(test))
 
     return result
 
@@ -83,7 +96,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         exit('Incorrect parameters. Only outfile needs to be provided')
     train = load_data("digits_train.jsonlines")
-    test = load_data("digits_test.jsonlines")
+    test = None # load_data("digits_test.jsonlines")
     prediction = build_prediction(train, test)
     with open(sys.argv[1], "wt") as out:
         for x, label in zip(test, prediction):
